71b8ecd68a515a70ac37fe587b29c0e526d47f68
niharsudani94@gmail.com <niharsudani94@gmail.com>
2025-05-11 18:02:50 +0530

    Updated product page layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Footer from './common/Footer';
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      
+      <Home />
       <Footer />
     </div>
   );
